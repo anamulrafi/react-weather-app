77b61c95fb01956f3bcef5669135d68088e89a0f
AnamulRafi <anamulhoquerafi118399@gmail.com>
2022-11-21 09:42:23 +0600

    seach bar created

---
+++ src/components/search.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+
+const Search = ({ onSearchChange }) => {
+    const [search, setSearch] = useState(null);
+    const handleOnChange = (searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData);
+    }
+    return (
+        <AsyncPaginate
+            placeholder="Search for city"
+            debounceTimeout={600} //600ms
+            value={search}
+            onChange={handleOnChange}
+        />
+    )
+
+}
+export default Search;
